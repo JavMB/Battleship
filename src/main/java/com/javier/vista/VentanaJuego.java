42dfbad9610a6563b16b0bd2bda4941d6a86a74a
Javier <javiermengual@live.com>
2025-05-10 22:59:16 +0200

    Añadidos paneles para tableros de jugador y enemigo

--- src/main/java/com/javier/vista/VentanaJuego.java
+++ src/main/java/com/javier/vista/VentanaJuego.java
@@ -5,6 +5,9 @@ import java.awt.*;
 
 public class VentanaJuego extends JFrame {
 
+    private JPanel panelJugador;
+    private JPanel panelEnemigo;
+
     public VentanaJuego() {
         super("BattleShip - Partida");
         this.setSize(1000, 600);
@@ -18,6 +21,27 @@ public class VentanaJuego extends JFrame {
 
         JPanel panelPrincipal = new JPanel();
         panelPrincipal.setLayout(new BorderLayout());
+
+
+        JPanel panelTableros = new JPanel();
+        panelTableros.setLayout(new GridLayout(1, 2, 20, 0));
+
+
+        panelJugador = new JPanel();
+        panelJugador.setBorder(BorderFactory.createTitledBorder("Tu flota"));
+        panelJugador.setLayout(new GridLayout(10, 10));
+
+
+        panelEnemigo = new JPanel();
+        panelEnemigo.setBorder(BorderFactory.createTitledBorder("Enemigo"));
+        panelEnemigo.setLayout(new GridLayout(10, 10));
+
+
+        panelTableros.add(panelJugador);
+        panelTableros.add(panelEnemigo);
+
+
+        panelPrincipal.add(panelTableros, BorderLayout.CENTER);
 
 
         this.add(panelPrincipal);
