adfbe8847237ef892119c601a4362efd38f52c4c
Mireya Serrano <mireyaserrano181105@gmail.com>
2025-05-25 22:01:48 +0200

    javaDocs Mireya

--- src/main/java/com/javier/modelo/Estrategia.java
+++ src/main/java/com/javier/modelo/Estrategia.java
@@ -3,6 +3,13 @@ package com.javier.modelo;
 import java.util.Set;
 
 public interface Estrategia {
+
+    /**
+     * Método con el que la CPU dispara
+     * @param enemigo El tablero del jugador
+     * @param yaDisparadas Las celdas que ya han sido disparadas
+     * @return La coordenada que se dispara
+     */
     Coordenada disparar(Tablero enemigo, Set<Coordenada> yaDisparadas);
 
 }
