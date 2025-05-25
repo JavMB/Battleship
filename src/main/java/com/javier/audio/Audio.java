bd128e7d582799c512acfed99eb998e880bda6f6
Josep Vicent Mestre Llobell <59211313+BYjosep@users.noreply.github.com>
2025-05-25 11:51:20 +0200

    refactor(Audio): Hace el campo 'clip' final

--- src/main/java/com/javier/audio/Audio.java
+++ src/main/java/com/javier/audio/Audio.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.net.URL;
 
 class Audio {
-    private Clip clip;
+    private final Clip clip;
 
     /**
      * Crea una instancia de Audio cargando un archivo .wav desde el classpath.
